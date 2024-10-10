15760a6f1b0f33713f1a310f6fbc7e1bbb119637
40692325 <40692325@live.napier.ac.uk>
2024-10-10 10:34:06 +0100

    Update Main.java

--- src/main/java/com/napier/semCW/Main.java
+++ src/main/java/com/napier/semCW/Main.java
@@ -9,22 +9,24 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-// Connect to MongoDB
+        // Connect to MongoDB on local system using the host "mongo-dbserver" and default port 27000
+        // MongoClient is the entry point to a MongoDB cluster
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
+        //Collections in MongoDB are similar to tables in SQL databases
         MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
+        // Create a document to store a name, class, year and a nested document for coursework (CW) and exam (EX) results
         Document doc = new Document("name", "Kevin Sim")
                 .append("class", "Software Engineering Methods")
                 .append("year", "2021")
                 .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
+        // Insert the document into the "test" collection
         collection.insertOne(doc);
 
-        // Check document in collection
+        // Retrieve the first document from the "test" collection and print it as a JSON string
+        //This is just to check if the document was successfully inserted
         Document myDoc = collection.find().first();
         System.out.println(myDoc.toJson());
     }
