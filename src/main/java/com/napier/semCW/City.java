674e6a3adbde0b1e701ff5c390044a1f02bf7e9a
Liam-Dev96 <144713745+Liam-Dev96@users.noreply.github.com>
2024-11-06 18:05:20 +0000

    initialise variables that match db structure

--- src/main/java/com/napier/semCW/City.java
+++ src/main/java/com/napier/semCW/City.java
@@ -1,4 +1,15 @@
 package com.napier.semCW;
 
 public class City {
+    // city's ID number
+    public int ID;
+    //city's Name
+    public String Name;
+    //city's Country code
+    public String CountryCode;
+    //city's district
+    public String District;
+    //city's population
+    public int Population;
+
 }
