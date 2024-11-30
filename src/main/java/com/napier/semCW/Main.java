ba8dc819afa1de3cde6e9f31f195c08595d4418a
Liam <40646739@live.napier.ac.uk>
2024-11-30 21:42:50 +0000

    updated main to produce report, and uploaded report

--- src/main/java/com/napier/semCW/Main.java
+++ src/main/java/com/napier/semCW/Main.java
@@ -69,10 +69,14 @@ public class Main {
         }
     }
 
-public ArrayList<Country> getCounCon(){
+    public ArrayList<Country> getCounCon() {
+        if (con == null) {
+            System.out.println("No database connection.");
+            return null;
+        }
         try {
             Statement countrycon = con.createStatement();
-            String ccltsQuery = "SELECT Name, Continent, Region, Population FROM country a order by a.Population DESC";
+            String ccltsQuery = "SELECT c.Name, c.Continent, c.Region, c.Population FROM country c order by c.Population DESC";
             ResultSet rsetcclts = countrycon.executeQuery(ccltsQuery);
             ArrayList<Country> countries = new ArrayList<>();
             while (rsetcclts.next()) {
@@ -89,50 +93,51 @@ public ArrayList<Country> getCounCon(){
             System.out.println("failed to get country details");
             return null;
         }
-}
-
-public void PrintCountry(ArrayList<Country> countries, String filename){
-
-
-   StringBuilder sb = new StringBuilder();
-
-   sb.append("| Country Name | Continent | Region | Population |");
-   sb.append("| --- | --- | --- | --- |");
-    //runs an output to show all countries and continents sorted by population large to small
-    for (Country coun : countries) {
-        if (coun == null) continue;
-        sb.append("| " + coun.Name + " |" +
-                "| " + coun.Continent + " |" +
-                "| " + coun.Region + " |" +
-                "| " + coun.Population + " |");
-    }
-    try {
-        new File("./reports/").mkdir();
-        BufferedWriter writer = new BufferedWriter(new FileWriter("./reports/" + filename));
-        writer.write(sb.toString());
-        writer.close();
-    } catch (IOException e) {
-        e.printStackTrace();
     }
 
-}
+    public void PrintCountry(ArrayList<Country> countries, String filename){
+
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("| Country Name | Continent | Region | Population |\r\n");
+        sb.append("| --- | --- | --- | --- |\r\n");
+        //runs an output to show all countries and continents sorted by population large to small
+        for (Country coun : countries) {
+            if (coun == null) continue;
+            sb.append("| " + coun.Name + " |" +
+                    " " + coun.Continent + " |" +
+                    " " + coun.Region + " |" +
+                    " " + coun.Population + " |\r\n");
+        }
+        try {
+            new File("./reports/").mkdir();
+            BufferedWriter writer = new BufferedWriter(new FileWriter("./reports/" + filename));
+            writer.write(sb.toString());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
         // Create new Application
-        Main a = new Main();
+        Main app = new Main();
 
         // Connect to database
         if (args.length < 1) {
-            a.connect("localhost:33060", 10000);
+            app.connect("localhost:33060", 10000);
         } else {
-            a.connect(args[0], Integer.parseInt(args[1]));
+            app.connect(args[0], Integer.parseInt(args[1]));
         }
-        ArrayList<Country> countries = a.getCounCon();  // Now it's a list of countries
-        a.PrintCountry(countries, "countries.md");
+
+        ArrayList<Country> countries = app.getCounCon();  // Now it's a list of countries
+        app.PrintCountry(countries, "countries.md");
 
 
         // Disconnect from database
-        a.disconnect();
+        app.disconnect();
 
 
     }
